d222213b463efef905e4f3c9bc867d9bbd7f9d7a
Abdelrahman <abdelrahman.ashraf.mohamed@gmail.com>
2018-05-25 18:04:29 +0200

    write unit testing for auth module

--- server/src/utils/__tests__/auth.todo.js
+++ server/src/utils/__tests__/auth.todo.js
@@ -1,27 +1,42 @@
+import {isPasswordAllowed, userToJSON} from '../auth'
+
+describe('isPasswordAllowed', () => {
+  const allowedPasswords = ['s5a68r2s6q3']
+  const disallowedPasswords = ['12345678', '', 'asasasasas']
+  allowedPasswords.forEach(pwd => {
+    it(`"${pwd}" should be allowed`, () => {
+      expect(isPasswordAllowed(pwd)).toBe(true)
+    })
+  })
+
+  disallowedPasswords.forEach(pwd => {
+    it(`"${pwd}" should be allowed`, () => {
+      expect(isPasswordAllowed(pwd)).toBe(false)
+    })
+  })
+})
+
 test('isPasswordAllowed only allows some passwords', () => {
-  // here's where I'll demo things for you :)
+  expect(isPasswordAllowed('')).toBe(false)
+  expect(isPasswordAllowed('fffffffffff')).toBe(false)
+  expect(isPasswordAllowed('88888888888')).toBe(false)
+  expect(isPasswordAllowed('s1d2f3f6g9t')).toBe(true)
 })
 
 test('userToJSON excludes secure properties', () => {
-  // Here you'll need to create a test user object
-  // pass that to the userToJSON function
-  // and then assert that the test user object
-  // doesn't have any of the properties it's not
-  // supposed to.
-  // Here's an example of a user object:
-  // const user = {
-  //   id: 'some-id',
-  //   username: 'sarah',
-  //   // ↑ above are properties which should
-  //   // be present in the returned object
-  //
-  //   // ↓ below are properties which shouldn't
-  //   // be present in the returned object
-  //   exp: new Date(),
-  //   iat: new Date(),
-  //   hash: 'some really long string',
-  //   salt: 'some shorter string',
-  // }
+  const safeUser = {
+    id: 'some-id',
+    name: 'sarah',
+  }
+  const user = {
+    ...safeUser,
+    exp: new Date(),
+    iat: new Date(),
+    hash: 'some really long string',
+    salt: 'some shorter string',
+  }
+  const jsonUser = userToJSON(user)
+  expect(jsonUser).toEqual(safeUser)
 })
 
 //////// Elaboration & Feedback /////////
@@ -33,8 +48,8 @@ test('userToJSON excludes secure properties', () => {
 /*
 http://ws.kcd.im/?ws=Testing&e=auth%20util&em=
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
